302777e1957b110de8a2d349eb45606186c896d1
Benjamin <benjamin@thegoodplace.nl>
2022-03-04 14:57:08 +0100

    Formik added to todo functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css'
 import { addDoc } from "firebase/firestore"
 import * as firestore from "firebase/firestore"
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
+import { Formik, Field, Form } from 'formik';
 
-import firebase, {db, auth} from './firebase'
+
+import firebase, {db} from './firebase'
+
+import Login from './components/Login'
 
 //import { collection, addDoc, getDocs } from "firebase/firestore" 
 
 function App() {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
   const [todos, setTodos] = useState([])
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const emailRef = useRef() 
-  const passwordRef = useRef() 
+  const [clients, setClients] = useState([])
+
 
   const today = new Date();
   const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -55,62 +54,38 @@ function App() {
     firestore.getDocs(firestore.collection(db, "todo")).then((querySnapshot) => {
       setTodos(querySnapshot.docs.map(doc => doc.data()))
     })   
-  },[title])
+  },[])
 
-  const addTodo = (e) => {
-    e.preventDefault()
-    addDoc(firestore.collection(db, 'todo'), {
-      title: title,
-      timestamp: dateTime,
-      description: description,
-      progress: "Feedback",
-      department: "Administration",
-    })
-    setTodos([...todos, title])
-    setTitle('')
-    setDescription('')
+  useEffect(() => {
+    firestore.getDocs(firestore.collection(db, "clients")).then((querySnapshot) => {
+      setClients(querySnapshot.docs.map(doc => doc.data()))
+    })   
+  },[])
+
+  const addTodo = (values) => {
+    console.log(values)
+    addDoc(firestore.collection(db, 'todo'), 
+      values
+    )
+    setTodos([...todos, values])
   }
 
     // Create a password-based account
-    function createUser() {
-      createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user
-        console.log(user)
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-        // ..
-      })
-    }
+    // function createUser() {
+    //   createUserWithEmailAndPassword(auth, email, password)
+    //   .then((userCredential) => {
+    //     // Signed in 
+    //     const user = userCredential.user
+    //     console.log(user)
+    //     // ...
+    //   })
+    //   .catch((error) => {
+    //     const errorCode = error.code
+    //     const errorMessage = error.message
+    //     // ..
+    //   })
+    // }
 
-    function handleChange() {
-      setEmail(emailRef.current?.value)
-    }
-
-    function signIn() {
-      alert('You have been logged in')
-
-      const email = emailRef.current?.value
-      const password = passwordRef.current?.value
-      
-      // Sign in a user with an email address and password
-      signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user
-        console.log(user)
-        setTodos(todos)
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-      })
-    }
   
 
   return (
@@ -123,33 +98,41 @@ function App() {
           {todo.description}</p>
         </div>
       ))}
-   
-      <form>
-        <input 
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-        placeholder="Title"
-        type="text"/>
-        <input 
-        value={description}
-        onChange={e => setDescription(e.target.value)}
-        placeholder="Description"
-        type="text"/>
-        <button onClick={addTodo}>Add Todo</button>
-      </form>
+
+
+      {clients.map((client, i) => (
+        <div key={i}>
+          <p>{client.title}</p>
+        </div>
+      ))}
+
+
+    <Formik
+      initialValues={{
+        title: '',
+        description: '',
+      }}
+      onSubmit={values => addTodo(values)}
+      // onSubmit={async (values) => {
+      //   await new Promise((r) => setTimeout(r, 500));
+      //   alert(JSON.stringify(values, null, 2));
+      // }}
+    >
+      <Form>
+        <label htmlFor="title">Title</label>
+        <Field id="title" name="title" placeholder="title" />
+
+        <label htmlFor="description">Description</label>
+        <Field id="description" name="description" placeholder="description" />
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+
 
       {/* <button onClick={addEntry}>add</button> */}
 
-      <input 
-      ref={emailRef}
-      onChange={handleChange}
-      type="email" 
-      placeholder="E-mailaddress" />
-      <input 
-      ref={passwordRef}
-      type="password" 
-      placeholder="Password" />
-      <button onClick={signIn} >Sign In</button>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth"
+import firebase, {auth} from '../firebase'
+
+
+export default function Login() {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const emailRef = useRef() 
+    const passwordRef = useRef() 
+
+    function signIn() {
+        console.log('signIn')
+        const email = emailRef.current?.value
+        const password = passwordRef.current?.value
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user
+            console.log('signInWithEmailAndPassword')
+            window.location.reload()
+        })
+        .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+        })
+    }
+    function handleChange() {
+        setEmail(emailRef.current?.value)
+    }
+
+    return (
+        <div>
+            <input 
+            ref={emailRef}
+            onChange={handleChange}
+            type="email" 
+            placeholder="E-mailaddress" />
+            <input 
+            ref={passwordRef}
+            type="password" 
+            placeholder="Password" />
+            <button onClick={signIn} >Sign In</button>
+        </div>
+    )
+}
